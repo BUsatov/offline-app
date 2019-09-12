879b6db9528326e6f5386b1e5521aaa4491a85f5
busatov <bohdan.usatov@gmail.com>
2019-09-12 20:36:26 +0300

    remove primary keys

--- backend/app/app/db_models/category.py
+++ backend/app/app/db_models/category.py
@@ -6,6 +6,5 @@ from app.db.base_class import Base
 
 class Category(Base):
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True,
-                  primary_key=True, nullable=False)
+    name = Column(String, index=True, unique=True, nullable=False)
     events = relationship("Event", back_populates="category")
--- backend/app/app/db_models/resource_type.py
+++ backend/app/app/db_models/resource_type.py
@@ -6,5 +6,5 @@ from app.db.base_class import Base
 
 class ResourceType(Base):
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True, primary_key=True)
+    name = Column(String, index=True, unique=True)
     resources = relationship("Resource", back_populates="resource_type")
