abbc730378c1fbd91a95b25ff12b35b000701c5a
busatov <bohdan.usatov@gmail.com>
2019-09-17 08:55:01 +0300

    f

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 // if (process.env.DEV) {
-axios.defaults.baseURL = "http://localhost:80";
+// axios.defaults.baseURL = "http://localhost:80";
 // }
 
 ReactDOM.render(<App />, document.getElementById("app"));
